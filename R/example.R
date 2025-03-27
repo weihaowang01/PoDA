fa0386bff9e6039510e402e84b553864b933564c
weihaowang01 <weihaowang@sjtu.edu.cn>
2025-03-27 09:47:39 +0800

    Update example.R

--- R/example.R
+++ R/example.R
@@ -6,7 +6,7 @@ example_data=readRDS("~~/PODA/R/example_data.rds")  #example data with 500 taxa 
 
 Y=example_data$count # observed abundance matrix
 u=example_data$interest # phenotype of interest
-confun=example_data$confun # all phenotyes (1st column is represents the phenotype of interest)
+confun=example_data$confun # all phenotyes (the first column represents the phenotype of interest)
 
 rej=PODA(Y = Y, u = u, confun = confun[,-1] ,rep_time=10,tau = 1,fdrnomial=0.05,fdrmethod="BH",lib_cut=1000, pre_cut=0.2) #PoDA
 
