948fbf9fee2ea37288e563d765881a0def1b6fd1
weihaowang01 <weihaowang@sjtu.edu.cn>
2025-09-02 17:32:36 +0800

    Add files via upload

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#'An example data used for test PoDA
+#'
+#' A dataset containing "count", "interest", "confoun"
+
+#'
+#' @format A  list with elements
+#' \describe{
+#'   \item{count}{count matrix, 500 taxa by 100 samples}
+#'   \item{interest}{the phenotype of interest, denoted as \code{u}}
+#'   \item{confoun}{two confounders, denoted as \code{z1} and \code{z2}}
+#' }
+"example_data"
+
+
